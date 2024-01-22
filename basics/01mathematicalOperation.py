70d42e6d4ec40e911c4d860eb056c6b463e492b4
km-saifullah <kmsaifullah16@gmail.com>
2024-01-22 12:51:05 +0600

    variable added

---
+++ basics/01mathematicalOperation.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 3
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 // number2)
+# print(number1 % number2)
+# print(number1 ** number2)
+# print(type(number1))
+
+# user input
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+result = num1 + num2
+print("The result is:",result)
