93481053b91cea0cc2802b193cc22790f7d143c8
km-saifullah <kmsaifullah16@gmail.com>
2024-01-26 21:48:33 +0600

    intro to turtle

---
+++ basics/04leapyear.py
@@ -0,0 +1,12 @@
+# find out leap year
+year = int(input('Enter a year:'))
+if year %4==0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+           print(year, 'is leap year')
+        else:
+            print(year,'is not leap year')
+    else:
+        print(year, 'is leap year')
+else:
+    print(year,'is not leap year')
