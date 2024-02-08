9428d136b37557998c4f15c00f921e96253c4e8d
km-saifullah <kmsaifullah16@gmail.com>
2024-02-08 20:59:03 +0600

    basics of for loop in python

---
+++ basics/06tutrtleExp.py
@@ -0,0 +1,17 @@
+# square
+base = int(input("Enter the length of base: "))
+angle=90
+
+import turtle 
+turtle.shape('circle')
+
+turtle.forward(base)
+turtle.left(angle)
+turtle.forward(base)
+turtle.left(angle)
+turtle.forward(base)
+turtle.left(angle)
+turtle.forward(base)
+turtle.left(angle)
+
+turtle.exitonclick()
---
+++ basics/07loop.py
@@ -0,0 +1,38 @@
+# basic for loop
+"""
+for i in range(5):
+    print(i,'Hello World')
+"""
+
+# generate a square using for loop
+"""
+import turtle
+turtle.shape('turtle')
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.exitonclick()
+"""
+
+# add fifty 1's using for loop
+# result = 0
+# for _ in range(50):
+#     result = result+1
+# print(result)
+
+# add 1 to 50
+# result = 0
+# for i in range(51):
+#     result = result+i
+# print(result)
+
+result = 0
+for i in range(1,51):
+    result = result+i
+print(result)
+
+for i in range(1,10,4):
+    print(i)
+
